ac57b81bdeefdd4aacd892b6dcf0a2b791da92fd
Esben Viskum <viskum@viskum.dk>
2021-04-06 12:54:19 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Esben Viskum on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
